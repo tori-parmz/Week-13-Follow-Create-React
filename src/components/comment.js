5352bdf2766f94a0d7c6b6bf0da974890e7c147e
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-03-18 15:51:52 -0500

    first comit

---
+++ src/components/comment.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReplyButton from './reply-button';
+import LikeButton from './like-button';
+
+let e = React.createElement;
+
+export default class Comment extends React.Component {
+    render() {
+        return e('div,',
+            {class: 'card w-75'},
+            e('div',{class: 'card-header bg-success text-white'},
+            'Username and Time'),
+            e('div', 
+            {class: 'card-body'},
+            'Comment content'),
+            e('div',
+            {class: 'card-footer'},
+            e((LikeButton), {}, null),
+            e('span', {}, ' '),
+            e(ReplyButton, {}, null)
+            )
+        );
+    }
+}
